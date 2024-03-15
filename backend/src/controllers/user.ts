4fde84ff6febd440ac3aac71b891118647cf2ba9
bhuvansagar12 <VadluriBhuvanSagar.Reddy@team.telstra.com>
2024-03-15 14:19:00 +0530

    small changes

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -4,7 +4,7 @@ import { student } from '../models/student';
 export const getAllUserTask = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
 
     const data = await student.findAll({
-        order: ["class"]
+        order: ["dob"]
     });
 
    
